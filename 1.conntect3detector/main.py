2a316c6ce064e3680b661fd97286db120bcb11a1
whitetac <9902abcd@gmail.com>
2022-11-28 18:09:23 +0900

    add test code

--- 1.conntect3detector/main.py
+++ 1.conntect3detector/main.py
@@ -8,35 +8,45 @@ model = models.Sequential()
 model.add(layers.Conv2D(64, (2, 2), activation='relu', input_shape=(3, 4, 1)))
 model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 model.add(layers.Flatten())
-model.add(layers.Dense(500, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 model.summary()
-datasetFile = open("3connect.txt", "r")
 
 # read dataset
-X = []
-Y = []
-datastring = datasetFile.readlines()
-for i in range(0, len(datastring), 5):
-    Ystring = datastring[i].replace("\n", "").replace(" ", "")
-    Y.append(float(Ystring))
-    tmp2 = []
-    for j in (datastring[i+1:i+4]):
-        tmp = []
-        for k in j:
-            if(k == "" or k == "\n"):
-                continue
-            tmp.append(float(k))
-        tmp2.append(tmp)
-    X.append(tmp2)
-X = np.array(X)
-Y = np.array(Y)
 
+
+def readDataset(path):
+    datasetFile = open(path, "r")
+    X = []
+    Y = []
+    datastring = datasetFile.readlines()
+    for i in range(0, len(datastring), 5):
+        Ystring = datastring[i].replace("\n", "").replace(" ", "")
+        Y.append(float(Ystring))
+        tmp2 = []
+        for j in (datastring[i+1:i+4]):
+            tmp = []
+            for k in j:
+                if(k == "" or k == "\n"):
+                    continue
+                tmp.append(float(k))
+            tmp2.append(tmp)
+        X.append(tmp2)
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+
+X, Y = readDataset("3connect.txt")
 # train model
 model.fit(X, Y, epochs=100)
 
 # test model
+X, Y = readDataset("3connect_test.txt")
+for i in X:
+    print(model.predict(np.array([i])))
+    print(i)
